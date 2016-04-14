0a40e445b87e35641fe9f91856cecd91f2e2e359
Ryan P.C. McQuen <ryan.q@linux.com>
2016-04-14 11:07:06 -0700

    use strict!

--- pgbw.js
+++ pgbw.js
@@ -1,4 +1,8 @@
 /* set the copyright year automatically */
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById("footerYear").textContent = new Date().getFullYear();
-});
+/*jslint browser:true, white:true*/
+(function () {
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("footerYear").textContent = new Date().getFullYear();
+  });
+}());
