1b0e409106c39868b94cdb3e8723f316f45d8f48
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-10-25 10:14:06 -0700

    4 spaces.

--- pgbw.js
+++ pgbw.js
@@ -1,8 +1,8 @@
 /* set the copyright year automatically */
 /*jslint browser:true, white:true*/
 (function () {
-  'use strict';
-  document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("footerYear").textContent = new Date().getFullYear();
-  });
+    'use strict';
+    document.addEventListener('DOMContentLoaded', function () {
+        document.getElementById("footerYear").textContent = new Date().getFullYear();
+    });
 }());
